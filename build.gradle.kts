77202ab4c5fac8a3fb7aa60c4a1c9372ac2dad8f
triskj0 <tristreflis@gmail.com>
2024-04-28 23:01:52 +0200

    restructure files
    to make it easier to compile by another user

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("java")
+}
+
+group = "me.jtris"
+version = ""
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(fileTree("libraries"))
+    implementation(files(
+        "libraries/com/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar",
+        "libraries/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar"))
+}
