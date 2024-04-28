99426c46b95c67ab0d1c49d2fd9f4a134b44e5cb
triskj0 <tristreflis@gmail.com>
2024-04-28 23:45:20 +0200

    change src/main/java to src/me/jtris

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,14 @@ dependencies {
         "libraries/com/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar",
         "libraries/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar"))
 }
+
+sourceSets {
+    main {
+        java {
+            srcDir("src/me/jtris")
+        }
+        resources {
+            srcDir("src/me/resources")
+        }
+    }
+}
